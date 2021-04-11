6b25dbbf460b9022419dcaf1bcf4f5aba7d1b111
Norton <kevin197011@outlook.com>
2021-04-12 03:08:39 +0800

    Update.

--- Formula/ip-api.rb
+++ Formula/ip-api.rb
@@ -1,12 +1,9 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class IpApi < Formula
   desc "check ip info command tool"
   homepage "https://github.com/kevin197011"
-  url "https://github.com/kevin197011/utils/raw/main/ip-api.tar.gz"
-  version "0.0.1"
-  sha256 "b3080f284d82254135e173da51e38181c220179da3240160797a1fe366767da5"
+  version "v20210412"
+  url "https://github.com/kevin197011/utils/raw/main/ip-api_#{version}.tar.gz"
+  sha256 "fb73079e0fcd20fd830b9b065eeaad09a2fd342ab077628ca859f363c7f938a9"
   license "MIT"
 
 
