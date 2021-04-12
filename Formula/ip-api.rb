863f645a775713dd7fbb384c8e0ce7c8585e6583
Norton <kevin197011@outlook.com>
2021-04-12 23:50:37 +0800

    Update.

--- Formula/ip-api.rb
+++ Formula/ip-api.rb
@@ -3,7 +3,7 @@ class IpApi < Formula
   homepage "https://github.com/kevin197011"
   version "v20210412"
   url "https://github.com/kevin197011/utils/raw/main/ip-api_#{version}.tar.gz"
-  sha256 "53ad6db38cb596436b45829877fef488c813fb0b09e22adfd8c52c8cd1af07b8"
+  sha256 "ebd78b1c5006b613553bad6b9bb4ffc508d1973d263f288567dd163ebc679428"
   license "MIT"
 
   def install
